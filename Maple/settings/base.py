836fce1b688259d81d13bcc2d3f273cd70179c90
Eddy <eddy15201@gmail.com>
2022-04-07 23:21:38 +0800

    修改語系

--- Maple/settings/base.py
+++ Maple/settings/base.py
@@ -185,7 +185,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'zh-hant'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'Asia/Taipei'
 
