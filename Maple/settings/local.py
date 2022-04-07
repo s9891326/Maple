0a5aa72326d1b2d59a2129497bb2ada5d8b6afa0
Eddy <eddy15201@gmail.com>
2022-04-07 20:50:18 +0800

    修改DB設定

--- Maple/settings/local.py
+++ Maple/settings/local.py
@@ -1,24 +1,27 @@
 from .base import *
 
 
-mysql = "mysql"
+db_host = "mysql"
 redis = "redis"
-port = "3306"
+db_port = "3306"
+db_name = env("DB_NAME", "maple")
+db_user = env("DB_USER", "dbuser")
+db_password = env("DB_PASSWORD", "riu405405")
 if DEBUG:
     if DEBUG.lower() == "true":
-        mysql = redis = "192.168.223.127"
-        port = "3305"
+        db_host = redis = "192.168.223.127"
+        db_port = "3305"
     else:
-        mysql = redis = "127.0.0.1"
+        db_host = redis = "127.0.0.1"
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'maple',  # 数据库名
-        'USER': 'dbuser',  # 你设置的用户名 - 非root用户
-        'PASSWORD': 'riu405405',  # # 换成你自己密码
-        'HOST': mysql,  # 注意：这里使用的是db别名，docker会自动解析成ip
-        'PORT': port,  # 端口
+        'NAME': db_name,
+        'USER': db_user,
+        'PASSWORD': db_password,
+        'HOST': db_host,  # 使用docker時這裡使用的是DB的別名，docker會自動解析成IP
+        'PORT': db_port,
     }
 }
