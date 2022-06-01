a04b1819db38c64faf1f78f13618d4611ed83ab8
Eddy <eddy15201@gmail.com>
2022-06-01 17:43:17 +0800

    1. 增加商品列name的查詢欄位 2. 增加註解和回傳格式

--- utils/convert_util.py
+++ utils/convert_util.py
@@ -1,7 +1,15 @@
+from typing import Dict, Any
+
 from config.convert_field_config import *
 
 
-def convert_field_to_sql_query(data: dict, converter):
+def convert_field_to_sql_query(data: dict, converter) -> Dict[str, Any]:
+    """
+    轉換欄位型態，讓該型態能直接進行ORM語法
+    :param data:
+    :param converter:
+    :return:
+    """
     sql_query = dict()
     for k, v in data.items():
         if hasattr(converter, k):
