852bce09b223a061dc9c15e665b857c42286e46d
Eddy <eddy15201@gmail.com>
2022-06-06 21:43:32 +0800

    測試時間點

--- worker/sched.py
+++ worker/sched.py
@@ -1,15 +1,10 @@
-import sys
-import os
-
 import logging
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 import django
-django.setup()
 
-# sys.path.insert(0, os.path.abspath('../'))
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Maple.settings.local')
+django.setup()
 
 from exchange.models import Product
 from utils.util import get_two_days_ago
@@ -17,11 +12,11 @@ from utils.util import get_two_days_ago
 sched = BlockingScheduler()
 
 
-# @sched.scheduled_job('interval', minutes=0, hours=22)
-@sched.scheduled_job('interval', seconds=10)
+@sched.scheduled_job('interval', minutes=46, hours=21)
+# @sched.scheduled_job('interval', seconds=10)
 def delete_time_out_product():
     logging.warning('Delete_time_out_product')
-    # Product.objects.filter(update_date__lte=get_two_days_ago()).delete()
+    Product.objects.filter(update_date__lte=get_two_days_ago()).delete()
 
 
 sched.start()
