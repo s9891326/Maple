52081548743f1dc4106f8bda2107b6b24c6fe934
Eddy <eddy15201@gmail.com>
2022-06-06 21:50:39 +0800

    測試時間點

--- worker/sched.py
+++ worker/sched.py
@@ -12,8 +12,8 @@ from utils.util import get_two_days_ago
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=46, hours=21)
 # @sched.scheduled_job('interval', seconds=10)
+@sched.scheduled_job('interval', minutes=55, hours=21)
 def delete_time_out_product():
     logging.warning('Delete_time_out_product')
     Product.objects.filter(update_date__lte=get_two_days_ago()).delete()
