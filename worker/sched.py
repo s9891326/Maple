baf499bca83e698fcd07516cd85a3393321283be
Eddy <eddy15201@gmail.com>
2022-06-07 15:44:54 +0800

    修改為最終執行時間

--- worker/sched.py
+++ worker/sched.py
@@ -14,11 +14,10 @@ from utils.util import get_two_days_ago
 sched = BlockingScheduler(timezone=base.TIME_ZONE)
 
 
-@sched.scheduled_job('cron', second='*/10')
-# @sched.scheduled_job('cron', hour='0', minute='0', second='0')
+@sched.scheduled_job('cron', hour='0', minute='0', second='0')
 def delete_time_out_product():
     logging.warning('Delete_time_out_product: %s', get_two_days_ago())
-    # Product.objects.filter(update_date__lte=get_two_days_ago()).delete()
+    Product.objects.filter(update_date__lte=get_two_days_ago()).delete()
 
 
 sched.start()
