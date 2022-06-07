bba8205fa8b0667bf43f9d2891816f5fc387a163
Eddy <eddy15201@gmail.com>
2022-06-07 14:48:23 +0800

    測試時間

--- worker/sched.py
+++ worker/sched.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -15,9 +16,9 @@ sched = BlockingScheduler()
 # @sched.scheduled_job('interval', seconds=10)
 # @sched.scheduled_job('interval', minutes=10, hours=14)
 # @sched.scheduled_job('cron', day='*', hour='1', minute='0', second='0')
-@sched.scheduled_job('cron', second='10')
+@sched.scheduled_job('cron', second='*/10')
 def delete_time_out_product():
-    logging.warning('Delete_time_out_product')
+    logging.warning('Delete_time_out_product: %s', datetime.datetime.now())
     # Product.objects.filter(update_date__lte=get_two_days_ago()).delete()
 
 
