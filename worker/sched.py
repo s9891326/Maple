1a99c23c08a527f25192be2b7e3a15633fdd926b
Eddy <eddy15201@gmail.com>
2022-06-07 14:44:41 +0800

    測試時間

--- worker/sched.py
+++ worker/sched.py
@@ -13,10 +13,12 @@ sched = BlockingScheduler()
 
 
 # @sched.scheduled_job('interval', seconds=10)
-@sched.scheduled_job('interval', minutes=10, hours=14)
+# @sched.scheduled_job('interval', minutes=10, hours=14)
+# @sched.scheduled_job('cron', day='*', hour='1', minute='0', second='0')
+@sched.scheduled_job('cron', second='10')
 def delete_time_out_product():
     logging.warning('Delete_time_out_product')
-    Product.objects.filter(update_date__lte=get_two_days_ago()).delete()
+    # Product.objects.filter(update_date__lte=get_two_days_ago()).delete()
 
 
 sched.start()
