b021f7c52e5d0e1fa9d25982cb538660fada4140
Eddy <eddy15201@gmail.com>
2022-06-27 22:10:56 +0800

    增加楓幣的後台

--- coins/admin.py
+++ coins/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from coins.models import Coin
+
+
+@admin.register(Coin)
+class CoinAdmin(admin.ModelAdmin):
+    list_display = ("title", "value", "total", "pay_method", "contact_method")
+    list_filter = ("pay_method", "contact_method")
+    search_fields = ("title", "pay_method",)
+    ordering = ("create_date",)
