aee37c11d6b9d67553a820d67d4f3beb92661e34
Eddy <eddy15201@gmail.com>
2022-09-18 20:27:32 +0800

    :bug: 移除重複問題

--- exchange/models.py
+++ exchange/models.py
@@ -59,7 +59,8 @@ class ProductList(models.Model):
                               choices=Career.choices, default=Career.Null)
     
     class Meta:
-        unique_together = ("category", "type", "stage_level", "name")
+        # Key (category, type, stage_level, name)=(armor, 護肩, 0, 皇家護肩) is duplicated.
+        # unique_together = ("category", "type", "stage_level", "name")
         verbose_name = verbose_name_plural = "商品列"
     
     def __str__(self):
