c472646536aa9c1882b8a4842c578c225742f778
Eddy <eddy15201@gmail.com>
2022-10-05 23:33:40 +0800

    :fire: 增加fly設定

--- Maple/settings/fly.py
+++ Maple/settings/fly.py
@@ -3,6 +3,7 @@ import dj_database_url
 DATABASES = {}
 DATABASES["default"] = dj_database_url.config(conn_max_age=600)
 
+ROOT_URLCONF = "Maple.urls"
 CORS_ALLOW_ALL_ORIGINS = True
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
