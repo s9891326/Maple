b96fef21a24a21efe6f5aa5aab109728aa41b086
Eddy <eddy15201@gmail.com>
2022-10-06 00:22:49 +0800

    :fire: 恢復fly配置

--- Maple/settings/fly.py
+++ Maple/settings/fly.py
@@ -1,14 +1,14 @@
 from .base import *
-# import dj_database_url
-#
-# DATABASES = {}
-# DATABASES["default"] = dj_database_url.config(conn_max_age=600)
+import dj_database_url
+
+DATABASES = {}
+DATABASES["default"] = dj_database_url.config(conn_max_age=600)
 
 ROOT_URLCONF = "Maple.urls"
 CORS_ALLOW_ALL_ORIGINS = True
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-import django_heroku
-
-django_heroku.settings(locals(), allowed_hosts=False, secret_key=False)
+# import django_heroku
+#
+# django_heroku.settings(locals(), allowed_hosts=False, secret_key=False)
