edf5e180d1f7dc4c4d29962a0d327c9b28c6c5a1
Eddy <eddy15201@gmail.com>
2022-10-12 22:07:25 +0800

    修改github.yml

--- Maple/settings/local.py
+++ Maple/settings/local.py
@@ -7,12 +7,12 @@ db_port = "3306"
 db_name = env("DB_NAME", "maple")
 db_user = env("DB_USER", "dbuser")
 db_password = env("DB_PASSWORD", "riu405405")
-if DEBUG:
-    if DEBUG.lower() == "true":
-        db_host = redis = "192.168.223.127"
-        db_port = "3305"
-    else:
-        db_host = redis = "127.0.0.1"
+
+if DEBUG and DEBUG.lower() == "true":
+    db_host = redis = "192.168.223.127"
+    db_port = "3305"
+else:
+    db_host = redis = "127.0.0.1"
 
 
 DATABASES = {
