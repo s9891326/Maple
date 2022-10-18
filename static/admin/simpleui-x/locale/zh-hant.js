5cef08e2743f0745b1e21f6a7cf5af1960fa0d47
Eddy <eddy15201@gmail.com>
2022-10-18 11:28:24 +0800

    Change language_code to 'zh-hant'

---
+++ static/admin/simpleui-x/locale/zh-hant.js
@@ -0,0 +1,44 @@
+var Lanuages = {
+    "Refresh": "刷新",
+    "Close current": "關閉當前",
+    "Close other": "關閉其他",
+    "Close all": "全部關閉",
+    "Open in a new page": "新標籤打開",
+    "Change theme": "改變主題",
+    "Default": "默認",
+    "Servers": "服務器",
+    "Application information": "應用信息",
+    "Home page": "主頁",
+    "Report issue": "報告問題",
+
+    "Select": "選擇",
+    "Selected": "已選擇",
+
+    "Purple": "紫色",
+    "Gray": "灰色",
+    "Dark green": "墨綠",
+    "Orange": "橙色",
+    "Black": "黑色",
+    "Green": "綠色",
+    "Light": "淡雅",
+
+
+    "Number": "編號",
+    "Theme name": "主題名",
+    "Action": "操作",
+
+    "ConfirmYes": "確定",
+    "ConfirmNo": "取消",
+    "Tips": "提示",
+    "Are you sure you want them all closed": "你確定要全部關閉嗎？",
+
+    "to": "至",
+    "Quick navigation": "快捷操作",
+    "Go back": '返回',
+
+    'Set font size': '設置字體大小',
+    'Reset': '重置',
+    'Are you sure you want to delete the selected?': '你確定要刪除選中的嗎？ ',
+    "Please select at least one option!": "請選中至少一個選項！",
+    "Please enter your username or password!": "請輸入您的用戶名或密碼！"
+}
