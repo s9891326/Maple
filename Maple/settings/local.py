17af3040c31d296e3b4744ab91aa94139d55fb08
Eddy <eddy15201@gmail.com>
2023-02-16 15:14:47 +0800

    add django loki

--- Maple/settings/local.py
+++ Maple/settings/local.py
@@ -57,6 +57,11 @@ LOGGING = {
         'simple': {
             'format': '%(levelname)s %(message)s'
         },
+        'loki': {
+            'class': 'django_loki.LokiFormatter',  # required
+            'format': '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] [%(funcName)s] %(message)s',  # optional, default is logging.BASIC_FORMAT
+            'datefmt': '%Y-%m-%d %H:%M:%S',  # optional, default is '%Y-%m-%d %H:%M:%S'
+        },
     },
     'handlers': {
         'console': {
@@ -72,12 +77,29 @@ LOGGING = {
             'backupCount': 5,
             'formatter': 'standard',
         },
+        'loki': {
+            'level': 'DEBUG',  # required
+            'class': 'django_loki.LokiHttpHandler',  # required
+            'host': '192.168.223.127',  # required, your grafana/Loki server host, e.g:192.168.57.242
+            'formatter': 'loki',  # required, loki formatter,
+            'port': 3100,  # optional, your grafana/Loki server port, default is 3100
+            'timeout': 0.5,  # optional, request Loki-server by http or https time out, default is 0.5
+            'protocol': 'http',  # optional, Loki-server protocol, default is http
+            'source': 'Loki',  # optional, label name for Loki, default is Loki
+            'src_host': 'localhost',  # optional, label name for Loki, default is localhost
+            'tz': 'UTC',  # optional, timezone for formatting timestamp, default is UTC, e.g:Asia/Shanghai
+        },
     },
     'loggers': {
         'main': {
             'handlers': ['console', 'main_file'],
             'level': 'WARNING',
             'propagate': False
+        },
+        'loki': {
+            'handlers': ['console', 'main_file', 'loki'],
+            'level': 'INFO',
+            'propagate': False,
         },
     }
 }
